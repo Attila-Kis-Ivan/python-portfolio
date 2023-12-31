cf164dddd5541a9ebe615241f3d3595385538c38
Attila Kis-Ivan <attilakis-ivan@Attilas-iMac.local>
2023-12-31 09:56:30 +0000

    added description on the new columns

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ with col1:
     st.image("images/home-img.svg")
     
 with col2:
-    st.title("Attila Kis-Ivan")
+    st.header("Attila Kis-Ivan")
     content ="""
     Hi everyone, I'm Attila Kis-Ivan a self-taught web developer with degree of MSc Engineering Management from London UK. I fell in love with programming in 2020. Currently I'm developing products with React andReact-Native. I'm interested in AI so Python and PyTorch are on the way 😊.
     """
     st.info(content)
     
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+
+"""    
+st.write(content2)
