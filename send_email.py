c75c8f1ec11901070e1a9971df4701188925b8bc
Attila Kis-Ivan <attilakis-ivan@Attilas-iMac.local>
2024-01-01 17:57:25 +0000

    adding enviroment variable

--- send_email.py
+++ send_email.py
@@ -1,11 +1,12 @@
 import smtplib, ssl
+import os
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
     username = "youremail@gmail.com"
-    password = "yourpassword"
+    password = os.getenv("PASSWORD")
 
     receiver = "youremail@gmail.com"
     context = ssl.create_default_context()
